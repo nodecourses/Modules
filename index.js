af953441063ccdb02b81c6874cd9f70593447ad6
Jan Biasi <biasijan@gmail.com>
2015-10-21 15:46:54 +0200

    Adding content :gem:

---
+++ index.js
@@ -0,0 +1,9 @@
+var action;
+
+/**
+ * Import the action module from /modules
+ * with common.js statements. Do not change
+ * the code given.
+ */
+
+action.main();
---
+++ modules/action.js
@@ -0,0 +1,10 @@
+exports.test = function() {
+    return !!(1 > 2);
+};
+
+/**
+ * Adding a method main to the exports
+ * which will do anything like log something
+ * to the console etc. It's your choice.
+ * Do not change the code given
+ */
